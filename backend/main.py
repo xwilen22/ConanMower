9f4886879885b76e963e0c8d007a0129802bb5b7
William <xwilen22@gmail.com>
2021-04-16 09:54:51 +0200

    Added serialiser class to main

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import firebaseClient as fc
+import mowerSerialiser as ms
 
 print("Doing the save")
-
 traveledPathClient = fc.FirebaseClient(fc.TraveledPathSettings())
 
 path = fc.TraveledPathData(20, 30, False)
