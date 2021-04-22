6ae3c8cf7ce1c7babc68f2178f7654645007fcac
Kim Berg <kim.berg99@hotmail.com>
2021-04-22 16:58:25 +0200

    the mowerSeraliser file now consists of a SerialConnection class. Also added documentation to it.

--- backend/main.py
+++ backend/main.py
@@ -2,23 +2,30 @@ import firebaseClient as fc
 import mowerSerialiser as ms
 import data
 
-### A connection to the database is established and used to insert an item in the database.
-### This is currently test code that will be built upon. --- This comment can most likely be removed later. ---
+### Initialize client firebase client and serial connection.
 
 traveledPathClient = fc.FirebaseClient(fc.TraveledPathSettings())
 
 serialPort = "/dev/ttyS0" # ttyS0 is used for Raspberry Pi Zero. Normally it would've been ACM0. --- NOTE: This is a temporary comment that can be removed on release ---
 
-serialConnection = ms.getSerialConnection(serialPort)
+serialConnection = ms.SerialConnection(serialPortString)
+
+print(data)
 
 while True:
-    serialConnection.write("Say something: ") # Temporary for test environment
-    rcv = ms.getObjectOnRecieve(serialConnection)
-    serialConnection.write("You sent: " + repr(rcv)) # Temporary for test environment
+    
+    byteArray = serialConnection.getBytesOnRecieve()
+    mowerData = serialConnection.getDataClass(byteArray).getDictionary()
+
+    traveledPathClient.InsertItem(mowerData)
+
+    #serialConnection.write("Say something: ") # Temporary for test environment
+    #rcv = serialConnection.getBytesOnRecieve() # Temporary for test environment
+    #serialConnection.write("You sent: " + repr(rcv)) # Temporary for test environment
 
     # TODO:
-    # 1. If the code above works, we need to convert the data to the correct format
-    # 2. Add the converted data as parameters to data.TraveledPathData(), e.g. path = fc.TraveledPathData(currentAngle, traveledDistance, endedByBorder)
+    # 1. If the code above works, we need to convert the data to the correct format (DONE)
+    # 2. Add the converted data as parameters to data.TraveledPathData(), e.g. path = fc.TraveledPathData(currentAngle, traveledDistance, endedByBorder) (SHOULD BE DONE)
 
 path = data.traveledPath.TraveledPathData(20, 30, False)
 traveledPathClient.InsertItem(path.getDictionary())
--- backend/mowerSerialiser.py
+++ backend/mowerSerialiser.py
@@ -1,31 +1,34 @@
-# This is a placeholder for serialiser class.
-# Parses data coming in from serial port to data class instance
 import serial
 import sys
 import data.traveledPath as traveledPathData
 
 BYTE_ORDER = sys.byteorder
 
-def getSerialConnection(serialPortString):
-    return serial.Serial(serialPortString, baudrate=9600, timeout=3.0)    
+### This class sets up and handles a serial connection between the Raspberry and the mower. 
+class SerialConnection():
 
-def getBytesOnRecieve(serialConnection):
-    return list(
-        serialConnection.read(1), #Left or right
-        serialConnection.read(1), #Relative angle change
-        serialConnection.read(2), #Distance
-        serialConnection.read(1)  #Stopped because of border
-    )
+    ## Creates the connection through a specified serial port.
+    def __init__(self, serialPortString):
+        self.port = serial.Serial(serialPortString, baudrate=9600, timeout=3.0)    
 
-### Parses a list of bytes to a TraveledPath data class
-def getDataClass(retrievedBytesList):
-    print(retrievedBytesList)
-    turnedLeft = int.from_bytes(retrievedBytesList[0], byteorder=BYTE_ORDER) == 1
-    angleChange = int.from_bytes(retrievedBytesList[1], byteorder=BYTE_ORDER)
+    ## This method reads the data coming in from the arduino and returns it as a list.
+    def getBytesOnRecieve(self):
+        return list(
+            self.port.read(1), #Left or right
+            self.port.read(1), #Relative angle change
+            self.port.read(2), #Distance
+            self.port.read(1)  #Stopped because of border
+        )
 
-    # Dunno if negative angle is for turning left or vise versa but ye
-    relativeAngle = angleChange * -1 if turnedLeft else angleChange
-    traveledDistance = int.from_bytes(retrievedBytesList[2], byteorder=BYTE_ORDER)
-    stoppedByObstacle = int.from_bytes(retrievedBytesList[3], byteorder=BYTE_ORDER)
+    ## Parses a list of bytes to a TraveledPath data class. The data class is returned.
+    def getDataClass(self, retrievedBytesList):
+        print(retrievedBytesList) # Kept for testing purposes.
+        turnedLeft = int.from_bytes(retrievedBytesList[0], byteorder=BYTE_ORDER) == 1
+        angleChange = int.from_bytes(retrievedBytesList[1], byteorder=BYTE_ORDER)
 
-    return traveledPathData.TraveledPathData(relativeAngle, traveledDistance, stoppedByObstacle)
+        # Dunno if negative angle is for turning left or vise versa but ye
+        relativeAngle = angleChange * -1 if turnedLeft else angleChange
+        traveledDistance = int.from_bytes(retrievedBytesList[2], byteorder=BYTE_ORDER)
+        stoppedByObstacle = int.from_bytes(retrievedBytesList[3], byteorder=BYTE_ORDER)
+
+        return traveledPathData.TraveledPathData(relativeAngle, traveledDistance, stoppedByObstacle)
