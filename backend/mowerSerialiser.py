227ea52fbbc487a86592632b138fd1b5840cc9c3
William <xwilen22@gmail.com>
2021-04-22 16:28:42 +0200

    Implemented parser

--- backend/mowerSerialiser.py
+++ backend/mowerSerialiser.py
@@ -1,15 +1,41 @@
 # This is a placeholder for serialiser class.
 # Parses data coming in from serial port to data class instance
 import serial
+import sys
 import data.traveledPath as traveledPathData
+
+BYTE_ORDER = sys.byteorder
 
 def getSerialConnection(serialPortString):
     return serial.Serial(serialPortString, baudrate=9600, timeout=3.0)    
 
-def getObjectOnRecieve(serialConnection):
-    return serialConnection.read(5)
+def getBytesOnRecieve(serialConnection):
+    return list(
+        serialConnection.read(1), #Left or right
+        serialConnection.read(1), #Relative angle change
+        serialConnection.read(2), #Distance
+        serialConnection.read(1)  #Stopped because of border
+    )
 
-# Returns parsed 
-def getDataClass(bytearray):
-    traveledPathData.TraveledPathData(11, 11, False)
-    pass
+### Parses a list of bytes to a TraveledPath data class
+def getDataClass(retrievedBytesList):
+    print(retrievedBytesList)
+    turnedLeft = int.from_bytes(retrievedBytesList[0], byteorder=BYTE_ORDER) == 1
+    angleChange = int.from_bytes(retrievedBytesList[1], byteorder=BYTE_ORDER)
+
+    # Dunno if negative angle is for turning left or vise versa but ye
+    relativeAngle = angleChange * -1 if turnedLeft else angleChange
+    traveledDistance = int.from_bytes(retrievedBytesList[2], byteorder=BYTE_ORDER)
+    stoppedByObstacle = int.from_bytes(retrievedBytesList[3], byteorder=BYTE_ORDER)
+
+    return traveledPathData.TraveledPathData(relativeAngle, traveledDistance, stoppedByObstacle)
+
+print(int.from_bytes(b'\x01\x01', byteorder=BYTE_ORDER))
+
+byteList = [
+    bytes([1]),
+    bytes([122]),
+    bytes([1, 1]),
+    bytes([0])
+]
+print(getDataClass(byteList).getDictionary())
