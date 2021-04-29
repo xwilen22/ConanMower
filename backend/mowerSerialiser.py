990a03e5a0b068fa26dce4efd04af161742bc089
Kim Berg <kim.ber99@hotmail.com>
2021-04-29 13:32:09 +0200

    implemented updated serial code

--- backend/mowerSerialiser.py
+++ backend/mowerSerialiser.py
@@ -9,23 +9,34 @@ class SerialConnection():
 
     ## Creates the connection through a specified serial port.
     def __init__(self, serialPortString):
-        self.port = serial.Serial(serialPortString, baudrate=9600, timeout=1.0)    
+        self.port = serial.Serial(serialPortString, baudrate=9600) 
+        self.port.flush()
+
+    def readInt(self, nrOfBytes, endian='big'):
+        return int.from_bytes(self.port.read(nrOfBytes), byteorder=endian)
 
     ## This method reads the data coming in from the arduino and returns it as a list.
     def getBytesOnRecieve(self):
-        startByte = self.port.read(1)
-        while int.from_bytes(startByte, byteorder=BYTE_ORDER) != 254:
-            startByte = self.port.read(1)
-        
-        print("start byte: ", startByte)  
 
-        returningList = []
+        startByte = 254
+        expectedNrOfBytes = 6
+        buffer = []
 
-        while len(returningList) < 6:
+        if self.port.inWaiting() >= expectedNrOfBytes and self.readInt(1) == startByte:
+            buffer.append( self.readInt(1) ) # turned left
+            buffer.append( self.readInt(1) ) # relative turn (degrees)
+            buffer.append( self.readInt(2) ) # distance (centimeters)
+            buffer.append( self.readInt(1) ) # stopped because of obstacle
+
+            return buffer
+
+        else:
+            return None
+        '''while len(returningList) < 6:
             dataToBeStored = self.port.read(1)
             if(dataToBeStored != b''):
                 returningList.append(dataToBeStored)
-        '''leftOrRight = self.port.read(1)
+        leftOrRight = self.port.read(1)
         angleChange = self.port.read(1)
         
         distanceOne = self.port.read(1)
@@ -39,10 +50,10 @@ class SerialConnection():
             distanceOne, #Distance
             distanceTwo,
             stoppedForObstacle  #Stopped because of border
-        ]'''
+        ]
 
         print("Bytes stored in the list: ", returningList)
-        return returningList
+        return returningList'''
 
     ## Parses a list of bytes to a TraveledPath data class. The data class is returned.
     def getDataClass(self, retrievedBytesList):
