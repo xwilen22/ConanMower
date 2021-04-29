005bbcb840a0e58be4370174f5cbdda8c90f6c2b
William <xwilen22@gmail.com>
2021-04-29 15:41:09 +0200

    CSS is served on load

--- backend/mapInterface.py
+++ backend/mapInterface.py
@@ -1,5 +1,5 @@
 import data.traveledPath
-from bottle import route, run, template
+from bottle import route, run, template, static_file
 
 HOST_PROPERTIES = {
     "port":8080,
@@ -9,5 +9,9 @@ HOST_PROPERTIES = {
 @route('/')
 def index():
     return template('index', name='Ringus')
+
+@route('/public/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='./public')
 
 run(host=HOST_PROPERTIES["name"], port=HOST_PROPERTIES["port"], debug=True)
