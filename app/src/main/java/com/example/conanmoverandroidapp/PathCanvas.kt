dad124dc13377a4f5916bc7781efed6533f4846d
EnGulSax <adamcarlleo@gmail.com>
2021-05-07 09:32:13 +0200

    Added temporary icons for obstacles´

--- app/src/main/java/com/example/conanmoverandroidapp/PathCanvas.kt
+++ app/src/main/java/com/example/conanmoverandroidapp/PathCanvas.kt
@@ -1,22 +1,27 @@
 package com.example.conanmoverandroidapp
 
+import android.R
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
-import android.view.MotionEvent
 import android.view.View
 import androidx.lifecycle.Observer
-import kotlin.random.Random
+
+
+data class Obstacle (
+    val bitmap: Bitmap,
+    val x: Float,
+    val y: Float, )
 
 
 class PathCanvas(context: Context, attributeSet: AttributeSet?) : View(context) {
     private var paint: Paint = Paint()
     private var path:  Path = Path()
-    private var screenHeight = 1000
-    private var screenWidth = 1000
+    private var obstacles = mutableListOf<Obstacle>()
+
+    /*TODO MOVE TO GLOBALS*/
+    private val WALL_OBSTACLE = 0
+    private val OBJECT_OBSTACLE = 1
 
     init {
         paint.isAntiAlias = true
@@ -40,7 +45,7 @@ class PathCanvas(context: Context, attributeSet: AttributeSet?) : View(context) 
     }*/
 
    /*PUT CODE TO TEST IN HERE FOR NOW*/
-    fun test() {
+   /* fun test() {
         val x = Random.nextInt(0, screenWidth) * (1 + Random.nextFloat())
         val y = Random.nextInt(0, screenHeight) *(1 + Random.nextFloat())
         val x2 = Random.nextInt(0, screenWidth) * (1 + Random.nextFloat())
@@ -60,22 +65,26 @@ class PathCanvas(context: Context, attributeSet: AttributeSet?) : View(context) 
         }
         invalidate()
         return true
-    }
+    }*/
 
-    private fun onUpdatePath(xStart: Float, yStart: Float, xStop: Float, yStop: Float){
+    private fun onUpdatePath(xStart: Float, yStart: Float, xStop: Float, yStop: Float, obstacle: Int){
         path.moveTo(xStart, yStart)
         path.lineTo(xStop, yStop)
-
+        obstacles.add(Obstacle(getBitMap(obstacle), xStop, yStop))
         invalidate()
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawPath(path, paint)
+        obstacles.forEach {
+            canvas.drawBitmap(it.bitmap, it.x, it.y, paint)
+        }
     }
 
+
     private fun drawPathOnCanvas(){
-        var startCoordinates = Coordinates(0.toFloat(), 0.toFloat())
+        var startCoordinates = Coordinates(width.toFloat()/2, height.toFloat()/2)
         Globals.traveledPathSessionList[0].traveledPaths.forEach {
             // Parse each data point
             val stopCoordinates = parseToCoordinates(
@@ -90,8 +99,8 @@ class PathCanvas(context: Context, attributeSet: AttributeSet?) : View(context) 
                 startCoordinates.xCoordinate,
                 startCoordinates.yCoordinate,
                 stopCoordinates.xCoordinate,
-                stopCoordinates.yCoordinate
-                //it.StoppedByObstacle
+                stopCoordinates.yCoordinate,
+                it.StoppedByObstacle
             )
 
             // Set new start coordinates from this data point, for the next data point
@@ -102,17 +111,27 @@ class PathCanvas(context: Context, attributeSet: AttributeSet?) : View(context) 
         }
     }
 
+
+    private fun getBitMap(obstacle: Int): Bitmap {
+       return when(obstacle){
+            WALL_OBSTACLE -> BitmapFactory.decodeResource(resources, R.drawable.checkbox_off_background)
+            OBJECT_OBSTACLE -> BitmapFactory.decodeResource(resources, R.drawable.sym_call_incoming)
+            else -> BitmapFactory.decodeResource(resources, R.drawable.ic_btn_speak_now)
+        }
+    }
+
+
     private fun parseToCoordinates(oldX: Float, oldY: Float, angle: Double, distance: Int) : Coordinates {
         // Convert angle in degree to angle in radians
         val angleInRadians = angle * Math.PI / 180
 
         // TODO: Adjust distance depending on screen-size
-        // var relativeDistance = This is some code
+        var relativeDistance = height/600
 
         // Calculate x and y coordinates for new point in map based on..
         // previous point, distance and angle
-        val newX = oldX + (distance * kotlin.math.cos(angleInRadians).toFloat())
-        val newY = oldY + (distance * kotlin.math.sin(angleInRadians).toFloat())
+        val newX = oldX + (distance * kotlin.math.cos(angleInRadians).toFloat()) * relativeDistance
+        val newY = oldY + (distance * kotlin.math.sin(angleInRadians).toFloat()) * relativeDistance
 
         return Coordinates(newX, newY)
     }
--- app/src/main/java/com/example/conanmoverandroidapp/ui/PathFragment.kt
+++ app/src/main/java/com/example/conanmoverandroidapp/ui/PathFragment.kt
@@ -28,4 +28,5 @@ class PathFragment : Fragment() {
         }
     }
 
+
 }
